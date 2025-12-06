1a3ba1b37fa9e8804d3475d73bce9af8bcda3247
niki <nikihidayati20@gmail.com>
2025-12-06 14:27:56 +0800

    update testimonies

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import HowItWorks from "@/components/HowItWorks";
 import EnergyStates from "@/components/EnergyStates";
-import Testimonials from "@/components/Testimonials";
 import Stats from "@/components/Stats";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
@@ -17,9 +16,9 @@ export default function Home() {
       <Features />
       <EnergyStates />
       <HowItWorks />
-      <Testimonials />
       <CTA />
       <Footer />
     </main>
   );
 }
+``
